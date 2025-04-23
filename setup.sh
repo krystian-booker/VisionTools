ceb9ad065cf38ecaa931ae083d6836cc1de2cc86
Krystian Booker <krystian.booker@gmail.com>
2025-04-22 22:08:20 -0400

    setup

--- setup.sh
+++ setup.sh
@@ -17,98 +17,52 @@ sudo apt install -y wget curl gpg apt-transport-https software-properties-common
 ####################################
 sudo locale-gen en_US en_US.UTF-8
 sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
-export LANG=en_US.UTF-8
 
 ####################################
 # Install VSCode
 ####################################
 echo "Installing VSCode..."
 if ! command -v code &> /dev/null; then
-    wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | sudo tee /etc/apt/keyrings/packages.microsoft.gpg > /dev/null
-    echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" | sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
+    wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+    sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
+    rm microsoft.gpg
+    sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
     sudo apt update
     sudo apt install -y code
 fi
 
 ####################################
-# Install Python
+# Install ROS
 ####################################
-echo "Installing Python..."
-if ! python3.10 --version &> /dev/null; then
-    sudo add-apt-repository -y ppa:deadsnakes/ppa
-    sudo apt update
-    sudo apt install -y python3.10 python3.10-venv python3.10-distutils
-    sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.10 1
+echo "Installing ROS Noetic..."
+
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
+sudo apt update
+sudo apt install -y ros-noetic-desktop-full
+
+source ~/.bashrc
+sudo apt install -y python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential
+sudo apt-get install -y ros-noetic-catkin
+
+# Setup environment
+echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+
+# Initialize rosdep
+if [ ! -f /etc/ros/rosdep/sources.list.d/20-default.list ]; then
+    sudo rosdep init
 fi
-
-####################################
-# AprilTag3 dependencies
-####################################
-sudo apt install -y libopencv-dev
-
-####################################
-# Install ROS 2 Humble
-####################################
-echo "Installing ROS2 Humble..."
-if [ ! -f /etc/apt/sources.list.d/ros2.list ]; then
-    sudo add-apt-repository universe -y
-    curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key | sudo tee /usr/share/keyrings/ros-archive-keyring.gpg > /dev/null
-    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
-    sudo apt update
-fi
-
-sudo apt install -y ros-humble-desktop python3-colcon-common-extensions \
-                    ros-humble-v4l2-camera ros-humble-image-transport ros-humble-cv-bridge \
-                    python3-rosdep
-
-# Add ROS to bashrc if not already there
-grep -qxF 'source /opt/ros/humble/setup.bash' ~/.bashrc || echo "source /opt/ros/humble/setup.bash" >> ~/.bashrc
-source /opt/ros/humble/setup.bash
-
-# GTSAM
-sudo apt install -y ros-$ROS_DISTRO-gtsam
-
-# Force shell rehash
-hash -r
-
-####################################
-# Setup ROS2 workspace
-####################################
-echo "Setting up ROS2 workspace..."
-WORKSPACE_DIR=~/ROS2FRC
-
-# Clone workspace if it doesn't exist
-if [ ! -d "$WORKSPACE_DIR" ]; then
-    git clone --recurse-submodules https://github.com/krystian-booker/ROS2FRC.git "$WORKSPACE_DIR"
-fi
-
-# Build AprilTag3
-echo "Building AprilTag3..."
-cd "$WORKSPACE_DIR/src/apriltag" || exit 1
-mkdir -p build && cd build || exit 1
-cmake ..
-make -j"$(nproc)"
-sudo make install
-sudo ldconfig
-
-# Source workspace setup file helper
-source_workspace() {
-    local setup_file="$1/install/setup.bash"
-    if [ -f "$setup_file" ]; then
-        source "$setup_file"
-    else
-        echo "Warning: Could not find setup file at $setup_file"
-    fi
-}
-
-# Back to workspace root
-cd "$WORKSPACE_DIR" || exit 1
-
-# Install dependencies and build full workspace
-echo "Resolving dependencies with rosdep..."
-sudo rosdep init 2>/dev/null || echo "rosdep already initialized"
 rosdep update
-rosdep install --from-paths src --ignore-src -r -y
 
-echo "Building full workspace..."
-colcon build --symlink-install
+#just testing
+mkdir -p ~/catkin_ws/src
+cd ~/catkin_ws/
+catkin_make
+source devel/setup.bash
+echo "source ~/catkin_ws/devel/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+cd ~/catkin_ws/src
+catkin_create_pkg beginner_tutorials std_msgs rospy roscpp
+cd ~/catkin_ws
+catkin_make
