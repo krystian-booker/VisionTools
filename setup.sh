c87caab5659315856a96cd7fedd9afd79ea9860a
Krystian Booker <krystian.booker@me.com>
2025-05-20 20:39:15 -0400

    updated install script for ROS2

--- setup.sh
+++ setup.sh
@@ -1,16 +1,45 @@
-#!/bin/bash
-set -e
+#!/usr/bin/env bash
+set -euo pipefail
 
-# Name of your workspace/repo directory
 REPO_NAME="VisionTools"
+LOCALE="en_US.UTF-8"
+ROS_KEYRING="/usr/share/keyrings/ros-archive-keyring.gpg"
+ROS_LIST="/etc/apt/sources.list.d/ros2.list"
 
-# Ask for sudo up front
+# keep sudo alive
 sudo -v
+trap 'sudo -v' EXIT
 
-echo "Installing all requirements..."
-sudo apt update && sudo apt upgrade -y
+echo "### Locale setup ###"
+sudo apt update -y
+sudo apt install -y locales
+sudo locale-gen "${LOCALE%.*}" "$LOCALE"
+sudo update-locale LC_ALL="$LOCALE" LANG="$LOCALE"
+export LANG="$LOCALE"
+locale
 
-####################################
-# Essentials
-####################################
-sudo apt install -y git
+echo "### System update & essentials ###"
+# upgrade & essentials in one go
+sudo apt upgrade -y
+sudo apt install -y git curl software-properties-common
+sudo add-apt-repository -y universe
+
+echo "### ROS 2 setup ###"
+# add key
+sudo curl -sSL \
+  https://raw.githubusercontent.com/ros/rosdistro/master/ros.key \
+  -o "$ROS_KEYRING"
+
+# add source
+arch=$(dpkg --print-architecture)
+codename=$(. /etc/os-release && echo "$UBUNTU_CODENAME")
+echo "deb [arch=$arch signed-by=$ROS_KEYRING] \
+  http://packages.ros.org/ros2/ubuntu $codename main" \
+  | sudo tee "$ROS_LIST" >/dev/null
+
+# final install
+sudo apt update -y
+sudo apt install -y ros-dev-tools ros-jazzy-desktop
+
+echo "source /opt/ros/jazzy/setup.bash" >> ~/.bashrc
+echo "### Done: '$REPO_NAME' ready! ###"
