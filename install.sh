6355f946a41e4e5be302faef075001a36b4bbe04
Krystian Booker <krystian.booker@gmail.com>
2025-06-16 22:12:49 -0400

    fixed user prompy

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e  # Exit on any error
 
-sudo apt update && sudo apt install locales
+sudo apt update && sudo apt install -y locales
 sudo locale-gen en_US en_US.UTF-8
 sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
 export LANG=en_US.UTF-8
